fd36ea5091945084fc59f5dd90a5f72618932674
奔跑的小草 <467195537@qq.com>
2023-06-03 12:34:50 +0800

    chor: 更新 docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/running-grass/bevy-tutorial/',
           lastVersion: 'current',
           disableVersioning: false,
           // includeCurrentVersion: true,
